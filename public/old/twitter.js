d075cddc3a457b9eae1c0c2906f966ec08a72ff1
Joey Kinnan <jmkappinv@gmail.com>
2025-05-09 15:20:27 -0400

    clearing space

--- public/old/twitter.js
+++
@@ -1,9 +0,0 @@
-function switchCSS(windowsize) {
-    if (windowsize > 800) {
-      $("#twitter").attr("data-height", "100");
-    } else {
-       $("#twitter").attr("data-height", "200"); 
-    }
-  }
-
-switchCSS(window)
